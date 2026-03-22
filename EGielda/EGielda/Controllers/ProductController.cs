646d4133881fc347099b06934aad508c8f1666fe
Adrian Kucharski <kucharskiadrian277@gmail.com>
2026-03-22 14:34:33 +0100

    updated css, layout, addes sidebar

---
+++ EGielda/EGielda/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EGielda.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["ShowSidebar"] = true;
+            return View();
+        }
+    }
+}
